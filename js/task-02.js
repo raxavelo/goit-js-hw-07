8ed1f4bfd1bd60bf4cbbbbe235258ec354ea65d3
raxavelo <raxavelo@gmail.com>
2024-11-07 18:13:22 +0100

    solved task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+  ingredientsList.appendChild(item);
+});
